b137554a3ad4ca02b454c54775e18f42071a9e2f
Rahul Bhagwat <rahulmbme@gmail.com>
2025-08-08 16:29:50 -0400

    add rotleft right macros

--- verilog/sha256.v
+++ verilog/sha256.v
@@ -13,4 +13,12 @@ typedef struct packed {
 	logic [63:0] bitlen; // instead of unsigned long long
 } SHA256_CTX;
 
+function automatic logic [31:0] rotleft(input logic [31:0] a, input int b);
+  rotleft = (a << b) | (a >> (32 - b));
+endfunction
+
+function automatic logic [31:0] rotright(input logic [31:0] a, input int b);
+  rotright = (a >> b) | (a << (32 - b));
+endfunction
+
 endmodule
