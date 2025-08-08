668de6b10e33e4979b5445ea32a9ab6fc3443f97
Rahul Bhagwat <rahulmbme@gmail.com>
2025-08-08 16:24:25 -0400

    inst clock and structs

--- verilog/main.v
+++ verilog/main.v
@@ -5,9 +5,21 @@ module main
   parameter int unsigned BENCHMARKSECONDS = 10
 )
 (
+  `ifdef SIMULATION_RUN
   input logic clk,
+  `endif
   input logic [7:0] input_buffer [64] // 64 byte contiguous
 );
+
+`ifdef FPGA_ICE40UP5K_RUN
+logic clk;
+// inst 12 mhz clock on ice40up5k, can be between:- 0b11 = 6 MHz to 0b00 = 48MHz
+SB_HFOSC #(.CLKHF_DIV("0b10")) hfosc_inst (
+    .CLKHFEN(1'b1),
+    .CLKHFPU(1'b1),
+    .CLKHF(clk)
+);
+`endif
 
 initial begin
   if ((BENCHMARKSECONDS > 600) || (BENCHMARKSECONDS < 0)) begin
--- verilog/sha256.v
+++ verilog/sha256.v
@@ -4,4 +4,13 @@ module sha256(
   input logic clk
 );
 
+typedef logic [7:0]  BYTE;   // 8-bit byte (char)
+typedef int unsigned WORD;   // 32-bit word, change to "long" for 16-bit machines
+
+typedef struct packed {
+  BYTE data[64];
+	WORD datalen, state[8];
+	logic [63:0] bitlen; // instead of unsigned long long
+} SHA256_CTX;
+
 endmodule
